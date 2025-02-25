3b828c79a7847a46585dd4f23b18fc99171c66ed
Nilton Filho <101483210+niltinfilho@users.noreply.github.com>
2025-02-25 01:41:33 -0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "sans": ["Inter", "sans-serif"],
+      },
+      boxShadow: {
+        "btn": "0 0 10px 10px #0FBAC044",
+        "btn-hover": "0 0 15px 15px #0FBAC044",
+      },
+    },
+    colors: {
+      "primary": "#0FBAC0",
+      "secondary": "#0FBAC0bb",
+      "tertiary": "#ffffff44",
+      "quaternary": "#cccccc",
+
+      "bg-primary": "#060606",
+      "bg-secondary": "#040404",
+      "bg-tertiary": "#111111",
+
+      "white": "#ffffff",
+    },
+    backgroundImage: {
+
+    }
+  },
+  plugins: [],
+};
