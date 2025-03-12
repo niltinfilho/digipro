c434d1b4876deb83ea5ce645e4a13e7d08c56afb
Nilton Filho <101483210+niltinfilho@users.noreply.github.com>
2025-03-12 17:16:38 -0300

    ajustando o menu na versao mobile

---
+++ scripts/dropdown-menu.js
@@ -0,0 +1,4 @@
+document.getElementById('mobile-menu-button').addEventListener('click', function () {
+  const mobileMenu = document.getElementById('mobile-menu');
+  mobileMenu.classList.toggle('hidden');
+});
